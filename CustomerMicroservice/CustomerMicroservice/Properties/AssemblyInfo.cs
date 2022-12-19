9640107900758d11e931d3de8060cd7ae34b44d3
Pietro Chiartano <chiartano.pietro@gmail.com>
2022-12-19 11:50:51 +0100

    Fix. Added working logger in the microservice

---
+++ CustomerMicroservice/CustomerMicroservice/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Runtime.InteropServices;
+
+// Nei progetti di tipo SDK come questo diversi attributi di assembly che sono stati
+// definiti cronologicamente in questo file vengono ora aggiunti automaticamente durante
+// la compilazione e popolati con i valori definiti nelle proprietà del progetto.
+// Per informazioni dettagliate sugli attributi inclusi e su come personalizzare questo
+// processo, vedere: https://aka.ms/assembly-info-properties
+
+
+// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili
+// ai componenti COM. Se è necessario accedere a un tipo in questo assembly da COM,
+// impostare su true l'attributo ComVisible per tale tipo.
+
+[assembly: ComVisible(false)]
+
+// Se il progetto viene esposto a COM, il GUID seguente verrà usato come ID di typelib.
+
+[assembly: Guid("bd528c1a-be62-4b84-9b0e-96698a1c5040")]
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
